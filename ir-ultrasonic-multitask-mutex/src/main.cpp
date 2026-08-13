d80813f58da3f5a620c09b5da5abe75533397a80
Parth Patel <parthp.tech@gmail.com>
2026-08-13 10:04:05 +0530

    [ir-ultrasonic-multitask--mutex] Add freertos for parallel tasking

--- ir-ultrasonic-multitask-mutex/src/main.cpp
+++ ir-ultrasonic-multitask-mutex/src/main.cpp
@@ -4,7 +4,98 @@
 #define TRIG_PIN 26
 #define ECHO_PIN 25
 
-GPIODriver irSensor(22); // TCRT5000
+GPIODriver irSensor(22);
+
+SemaphoreHandle_t dataMutex;
+
+struct SensorData
+{
+  bool obstacleDetected;
+  float distanceCm;
+};
+
+SensorData data;
+
+void irTask(void *parameter)
+{
+  while (1)
+  {
+    while (!xSemaphoreTake(dataMutex, portMAX_DELAY))
+    {
+    }
+    data.obstacleDetected = !irSensor.read();
+    xSemaphoreGive(dataMutex);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+};
+
+void ultrasonicTask(void *parameter)
+{
+  while (1)
+  {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+
+    digitalWrite(TRIG_PIN, LOW);
+    bool validReading = true;
+
+    long init_time = micros();
+    while (!digitalRead(ECHO_PIN))
+    {
+      if (micros() - init_time > 5000)
+      {
+        Serial.println("Unable to raise ECHO high");
+        validReading = false;
+        break; // exits the inner while loop
+      }
+    }
+
+    long start_time;
+    if (validReading)
+    {
+      start_time = micros();
+      while (digitalRead(ECHO_PIN))
+      {
+        if (micros() - start_time > 60000)
+        {
+          Serial.println("No object detected");
+          validReading = false;
+          break;
+        }
+      }
+    }
+
+    if (validReading)
+    {
+      long end_time = micros();
+      long duration = end_time - start_time;
+      long distance = duration * 0.0343 / 2;
+
+      xSemaphoreTake(dataMutex, portMAX_DELAY);
+      data.distanceCm = distance;
+      xSemaphoreGive(dataMutex);
+    }
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+};
+
+void monitorTask(void *parameter)
+{
+  while (1)
+  {
+    xSemaphoreTake(dataMutex, portMAX_DELAY);
+    Serial.print("Object Detected: ");
+    Serial.println(data.obstacleDetected);
+    Serial.print("Distance: ");
+    Serial.println(data.distanceCm);
+    xSemaphoreGive(dataMutex);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup()
 {
@@ -16,56 +107,13 @@ void setup()
   pinMode(ECHO_PIN, INPUT);
 
   digitalWrite(TRIG_PIN, LOW);
-}
 
-float calculate_distance()
-{
-  // Trigger ultrasonic sensor
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-
-  digitalWrite(TRIG_PIN, LOW);
-
-  long init_time = micros();
-  while (!digitalRead(ECHO_PIN))
-  {
-    if (micros() - init_time > 5000)
-    {
-      return -1;
-    }
-  };
-  long start_time = micros();
-
-  while (digitalRead(ECHO_PIN))
-  {
-    if (micros() - start_time > 60000)
-    {
-      return -1;
-    }
-  };
-
-  long end_time = micros();
-
-  long duration = end_time - start_time;
-
-  return duration * 0.0343 / 2;
+  dataMutex = xSemaphoreCreateMutex();
+  xTaskCreate(irTask, "IR Task", 2048, NULL, 1, NULL);
+  xTaskCreate(ultrasonicTask, "Ultrasonic Task", 2048, NULL, 1, NULL);
+  xTaskCreate(monitorTask, "Monitor Task", 2048, NULL, 1, NULL);
 }
 
 void loop()
 {
-
-  // Calculate distance in cm
-  float distance = calculate_distance();
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-
-  int isDetected = !irSensor.read();
-  Serial.print("Detected: ");
-  Serial.println(isDetected);
-
-  delay(60);
 }
